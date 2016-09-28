6f579c380fc949778aa3d6f793e4f9ae52dba987
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-09-28 14:43:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/articleone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
-app.get('/article-two', function (req, res) {
+app.get('/articletwo', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
-app.get('/article-three', function (req, res) {
+app.get('/articlethree', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
 });
 
