7b008911187e5bec93f5c3c6d5e5f78dda932440
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-09-30 15:28:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+var element=element.getElementById('main-text');
+element.innerHTML='This is a newexample on JS';
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveright(){
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft='px';
+}
+img.onclick=function(){
+    
+   var interval=setInterval(moveRight,50);
+};
