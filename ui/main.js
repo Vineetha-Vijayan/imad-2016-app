8ebf3ed608388b85e247901055fb4be5ce208a0a
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-02 21:30:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,13 @@ console.log('Loaded!');
 var element=element.getElementById('main-text');
 element.innerHTML='This is a newexample on JS';
 var img=document.getElementById('madi');
-var marginLeft=0;
+/*var marginLeft=0;
 function moveright(){
     marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft='px';
-}
+}*/
 img.onclick=function(){
+    img.style.marginLeft='100px';
     
-   var interval=setInterval(moveRight,50);
+  // var interval=setInterval(moveRight,50);
 };
