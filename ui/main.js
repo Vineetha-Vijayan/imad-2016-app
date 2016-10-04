c7ec5b8f12e7e1086ce832a4aa1f97ee00e5066a
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-04 13:13:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,18 @@ img.onclick=function(){
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
-counter=counter+1;
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+ if(request.readyState===XMLHttpRequest.DONE){
+     if(request.status===200){
+         var counter =request.responseText;
+       // counter=counter+1;
 var span= document.getElementById('count');
 span.innerHTML=counter.toString();
+     }
+ }   
+};
+request.open('GET','http://http://vineetha-vijayan.imad.hasura-app.io/counter',true);
+
+request.send(null);
 };
