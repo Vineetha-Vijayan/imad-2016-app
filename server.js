407e6bccbcd106826698d7715c84dbe8e3dfd1e7
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-05 12:43:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,12 @@ app.use(morgan('combined'));
             </p>`
    }
   };
-  
+ 
+ var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+}); 
 
 function createTemplate(data){
     var title=  data.title;
@@ -113,11 +118,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
+
 app.get('/:articleName', function (req, res) {
 var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
