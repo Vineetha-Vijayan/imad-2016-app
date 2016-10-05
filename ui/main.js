c3b1bb3868573f96c90622882d66fccf4fb81ac5
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-05 13:43:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,15 +40,25 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit-btn');
 submit.onclick=function(){
-    
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+ if(request.readyState===XMLHttpRequest.DONE){
+     if(request.status===200){
 var names=['name1','name2','name3','name4'];
 var list='';
 for(i=0;i<names.length;i++)
 var ul=document.getElementById('name-list');
 ul.innerHTML=list;
-
+}
+}
 
 
 };
+
+request.open('GET','http://vineetha-vijayan.imad.hasura-app.io/submit-name?name='+name,true);
+
+request.send(null);
+};
+
 
 
