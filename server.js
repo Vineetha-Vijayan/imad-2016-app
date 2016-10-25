0df5beb8ecaf6c118650e0d47404cda054924529
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-25 23:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db',function(req,res){
+    //make a select requst
+    
+    //return response with the result
+});
 
 var counter=0;
 app.get('/counter', function (req, res) {
