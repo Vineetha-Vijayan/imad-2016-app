57dcbcaab42a941b62f3d91fdb22974a90776f1b
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-27 15:43:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool=require('pg').Pool;
 var config={
     user:'vineetha-vijayan',
     database:'vineetha-vijayan',
-    host:''db.imad.hasura-app.io,
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
     
