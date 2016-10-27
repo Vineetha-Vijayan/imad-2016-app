9f8e5b599cf889ebfb9154d29c6131e57d144e46
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-27 15:58:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var config={
     database:'vineetha-vijayan',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD 
     
-}
+};
 
 var app = express();
 app.use(morgan('combined'));
