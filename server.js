ca99c3d3e10c6514439cc8b85669d720ce9433fe
Vineetha-Vijayan <job.vineethaarunnath@gmail.com>
2016-10-28 07:26:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,11 +150,29 @@ app.get('/submit-name',function(req,res)
     names.push(name);
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName', function (req, res) {
-var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+//var articleName=req.params.articleName;
+pool.query("SELECT * FROM article WHERE title="+ req.params.articleName, funcion(err,result){
+    if(err) {
+           
+           res.status(500).send(err.toString());
+       }
+       else{
+           
+           if(result.rows.length===0){
+               res.status(404).send('Article Not Found');
+           }
+           else{
+               var articleData=result.rows[0];
+               res.send(createTemlate(articleData));
+           }
+       }
+    });
+  //res.send(createTemplate(articles[articleName]));
  //res.send(createTemplate(articleOne));
 });
+
+
 
 
 
